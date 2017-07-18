4071a1ef949fee63e46810f56534757bbd308fff
iosdev <iosdev@iosdevnoMacBook-Pro.local>
2017-07-18 14:06:39 +0900

    Add project

---
+++ CryptoWithSwift/CryptoWithSwift.h
@@ -0,0 +1,19 @@
+//
+//  CryptoWithSwift.h
+//  CryptoWithSwift
+//
+//  Created by iosdev on 2017/07/18.
+//  Copyright © 2017年 iosdev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CryptoWithSwift.
+FOUNDATION_EXPORT double CryptoWithSwiftVersionNumber;
+
+//! Project version string for CryptoWithSwift.
+FOUNDATION_EXPORT const unsigned char CryptoWithSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CryptoWithSwift/PublicHeader.h>
+
+
